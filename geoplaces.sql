a8c5286d2ed0feb7a597fc34ed345a253f49c74a
chiqors <bandi_nada@live.com>
2020-03-31 22:06:08 +0700

    Missed the files index

--- geoplaces.sql
+++ geoplaces.sql
@@ -1,24 +1,7 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: May 06, 2019 at 04:15 AM
--- Server version: 5.7.26-0ubuntu0.18.04.1
--- PHP Version: 7.3.5-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `webgis`
---
 
 -- --------------------------------------------------------
 
